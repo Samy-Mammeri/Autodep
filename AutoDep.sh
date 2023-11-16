f85d59e293a7513e72505d345a6d62792d30330d
Samy <139016486+Samy-Mammeri@users.noreply.github.com>
2023-11-16 23:11:40 +0100

    this script sets up the architecture of the package and build builds it

--- AutoDep.sh
+++ AutoDep.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 # This script will setup the Debian package
-
 # In the first Place we must create the DEB package Architecture
- 
-# Directory where the script will operate
-FIELD=$(pwd)
 
 # Choosing the Package name
-echo "The AutoDep utility is currently running in this Directory, What do you want to Name your Package?"
+echo "The AutoDep utility is currently running in this Directory, Enter the name for your package:"
 read REP
 
 # Create the base directory for the package
@@ -20,4 +16,18 @@ mkdir -p $REP/DEBIAN
 # Create the usr/local/bin directory structure inside the package
 mkdir -p $REP/usr/local/bin
 
+# Then we create and configure the control file
+printf "Package: $REP\n
+Version: 1.0-0\n
+Section: utils\n
+Priority: optional\n
+Architecture: all\n
+Depends: bash (>= 4.0)\n
+Maintainer: Name <Your email>\n
+Description: Descripe your package\n" > $REP/DEBIAN/control
 
+# Get the Tools
+sudo apt-get install dpkg-dev debhelper devscripts
+
+# Build the Package with dpkg
+dpkg --build "$REP"
